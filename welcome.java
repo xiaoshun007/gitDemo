a45401b80e88e1b424b224ba9139a17cb0ae061b
xiaoshun <495314431@qq.com>
2015-07-26 17:01:43 +0800

    no message

---
+++ welcome.java
@@ -0,0 +1,11 @@
+public class welcome {
+	private String str;
+	
+	public setStr(String str) {
+		this.str = str;
+	}
+	
+	public static void main(Sring[] args) {
+		system.out.println("hello world!");
+	}
+}
